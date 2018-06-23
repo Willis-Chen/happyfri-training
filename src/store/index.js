a21213d2ae376a3446e8d17801b966a1af5c45ce
willischen(陈为立) <willischen@tencent.com>
2018-06-23 11:06:44 +0800

    Add vuex and static

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './action'
+import mutations from './mutations'
+
+
+Vue.use(Vuex)
+
+const state={
+
+}
+
+export default new Vuex.Store({
+  state,
+  actions,
+  mutations
+})
